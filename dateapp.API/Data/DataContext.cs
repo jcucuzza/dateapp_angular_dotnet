8ef99406eeda1e3dc85c0edc803fa8b059a533ff
Jonathan Cucuzza <smackthatcow@gmail.com>
2019-09-14 15:20:19 -0400

    Init Code

---
+++ dateapp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using dateapp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dateapp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ dateapp.API/README.md
@@ -0,0 +1,2 @@
+Develop within .Net and Angular 8
+currently using SQLite for database but will migrate later to a more production ready database.
