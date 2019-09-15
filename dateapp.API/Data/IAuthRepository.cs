2863de7675a36659aa6eb639b6d0f8f4e785223f
Jonathan Cucuzza <smackthatcow@gmail.com>
2019-09-15 11:30:10 -0400

    Added an Auth Interface to handle login, register, and existing user functions

---
+++ dateapp.API/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using dateapp.API.Models;
+
+namespace dateapp.API.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
--- dateapp.API/Models/User.cs
+++ dateapp.API/Models/User.cs
@@ -3,7 +3,7 @@ namespace dateapp.API.Models
     public class User
     {
         public int Id { set; get; }
-        public int Username { set; get; }
+        public string Username { set; get; }
         public byte[] PasswordHash { set; get; }
         public byte[] PasswordSalt { set; get; }
     }
