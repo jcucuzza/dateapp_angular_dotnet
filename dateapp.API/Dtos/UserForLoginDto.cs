22c74302af2a9dea0521f85670a243fc52b3ccb0
Jonathan Cucuzza <smackthatcow@gmail.com>
2019-09-15 16:28:29 -0400

    Added JWT  to login post

---
+++ dateapp.API/Dtos/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+namespace dateapp.API.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- dateapp.API/Dtos/UserForRegisterDtos.cs
+++ dateapp.API/Dtos/UserForRegisterDtos.cs
@@ -1,9 +1,15 @@
 //Data Transfer Object for User Registration.
+using System.ComponentModel.DataAnnotations;
+
 namespace dateapp.API.Dtos
 {
     public class UserForRegisterDtos
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 4, ErrorMessage = "Enter a password with no more than 10 characters and no less than 4")]
         public string Password { get; set; }
     }
 }
